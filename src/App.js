a5df3bedbc928952e610084bf4c161002756dff4
Vishnu Velayuthan <100023569@mvla.net>
2021-08-16 23:23:46 -0700

    Got API call for countries to work

--- src/App.js
+++ src/App.js
@@ -1,12 +1,79 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import "./App.css";
 
+//MaterialUI Dependencies
+import {
+  MenuItem,
+  FormControl,
+  Select, 
+  Card
+} from "@material-ui/core";
+
+//Gets list
+const get_url_countries_data = "https://disease.sh/v3/covid-19/countries"
+
+
 function App() {
+
+  const [countries, setCountries] = useState([]);
+  const [country, setCountry] = useState("worldwide");
+  //useEffect = runs piece of code based on condition
+  // ran once when component init 
+  // or when state is changed of var
+  useEffect(() => {
+    //Define async function
+    // Async function is basically waiting for promise to be fullfilled
+    const getCountryData = async() => { // fetch is built in JS func for get reqs
+      await fetch(get_url_countries_data) // await waits for the promise to be fullfilled
+        .then((res) => res.json())
+        .then((data) => { //Taking the JSON and then getting the specific info we want 
+          const countries = data.map(country => (
+            {
+              name: country.country, // United States, India, etc
+              value: country.countryInfo.iso2 // UK, IN, GER
+            }
+          )); 
+          setCountries(countries); //Setting state to this country
+        })
+    } 
+    getCountryData(); 
+  }, []) //This function is only called once at the init of the component
+
+
   return (
-    <div className="app">
-      <h1>
-        Lets do this COVID Stuff
-      </h1>
+    <div className="app"> {/* BEM naming convention component_element*/}
+      
+      {/* Header */}
+      <div className="app_header">
+
+        <h1>COVID-19 Tracker</h1> 
+
+        <FormControl className="app_dropdown"> {/* This is like a drop down */}
+          <Select variant="outlined" value={country}> 
+            <MenuItem value="worldwide">Worldwide</MenuItem>
+            {/* This loops through all the countries in state countries */}
+            {countries.map(country => 
+                <MenuItem value={country.value}>{country.name}</MenuItem>
+            )}
+          </Select>
+        </FormControl>
+        
+      </div>
+
+      
+      
+
+      {/* Title and Dropdown field */}
+
+      {/* Infobox */}
+      {/* Infobox */}
+      {/* Infobox */}
+
+      {/* Table */}
+      {/* Graph */} 
+
+      {/* Map */}
+
     </div>
   );
 }
