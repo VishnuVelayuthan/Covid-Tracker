c4e16a507ce5c0b3356c7906d706de54e7555993
Vishnu Velayuthan <100023569@mvla.net>
2021-08-24 16:04:52 -0700

    have some version of the graph working

--- src/LineGraph.js
+++ src/LineGraph.js
@@ -1,11 +1,116 @@
-import React from "react"; 
+
+import React, {useEffect, useState} from "react"; 
 import {Line} from "react-chartjs-2";
+import numeral from "numeral";
+
+const line_data_url = "https://disease.sh/v3/covid-19/historical/all?lastdays=120"
+
+const options = {
+    legend: {
+        display: false
+    },
+    elements: {
+        point: {
+            radius: 0
+        }
+    },
+    maintainAspectRatio: false,
+    tooltips: {
+        mode: "index",
+        intersect: false,
+        callbacks: {
+            label: function(tooltipItem, data) {
+                return numeral(tooltipItem.value).format("+0,0")
+            }
+        }
+    },
+    scales: {
+        xAxes: [
+            {
+                type: "time",
+                time: {
+                    format: "MM/DD/YY",
+                    tooltipFormat: "ll"
+                }
+            }
+        ],
+        yAxes: [
+            {
+                gridLines: {
+                    display: false
+                },
+                ticks: {
+                    callback: function(value, index, values) {
+                        return numeral(value).format("0a")
+                    }
+                }
+            }
+        ]
+    }
+}
+
+// This takes in our data object and what type of cases we want to graph
+const buildChartData = (data, casesType="cases") => {
+    const chartData = []; 
+    let lastDataPoint; 
+
+    // gets the "type" of cases list from the JSON
+    for (let date in data[casesType]) { 
+        if (lastDataPoint) {
+            const newDataPoint = {
+                x: date,
+                y: data[casesType][date] - lastDataPoint
+            }
+            chartData.push(newDataPoint); 
+        }
+        lastDataPoint = data[casesType][date];
+    };
+
+    return chartData; 
+}
 
 function LineGraph() {
+    const [data, setData] = useState({});
+
+    // useEffect to get data on the prev Covid cases
+    useEffect(() => {
+        const getChartData = async () => {
+            await fetch(line_data_url)
+            .then(res => res.json())
+            .then(data => {
+                const chartData = buildChartData(data);
+
+                console.log(chartData)
+
+                setData(chartData); 
+            });
+
+            console.log("now"); 
+            console.log(data); 
+        }
+        
+        getChartData(); 
+    }, []);
+    // console.log("hellO");
+    // console.log(data);
+
     return (
         <div>
-            <Line />
+            {data?.length > 0 && (
+                <Line 
+                    options={options}
+                    data={{
+                        datasets: [{
+                            backgroundColor: "rgba(204, 16, 52, 0.5)",
+                            borderColor: "#CC1034",
+                            data: data
+                        }]
+                    }} 
+                />
+            )}
         </div>
+            
+            
     );
 }
 
